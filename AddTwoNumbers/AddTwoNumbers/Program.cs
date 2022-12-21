c45338b3eba621ce693cd42be9b922a7542e735f
Marinus Holkema <marinusholkema@hotmail.com>
2022-12-21 12:56:55 +0100

    Done

---
+++ AddTwoNumbers/AddTwoNumbers/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Enter a number");
+var answer1 = Console.ReadLine();
+Console.WriteLine("Enter another number");
+var answer2 = Console.ReadLine();
+int.TryParse(answer1, out int number1);
+int.TryParse(answer2, out int number2);
+Console.WriteLine("The sum of these numbers is");
+Console.WriteLine(number1 + number2);
+
